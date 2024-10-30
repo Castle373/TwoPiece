71468488c195472cd3221ce238dc9e18391b9281
Diego Robles <125101098+Castle373@users.noreply.github.com>
2024-10-30 16:51:02 -0700

    InicioMapaWano

---
+++ Juego/tiled/items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="items" tilewidth="32" tileheight="32" tilecount="275" columns="25">
+ <image source="../recursos/img/items.png" width="800" height="367"/>
+</tileset>
